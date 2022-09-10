16618ffa9f28a65c4eb9cdd9c16611be2af611ee
Marcelo Cezario <marcelocezario@gmail.com>
2022-09-09 22:04:35 -0300

    Initial commit

---
+++ src/main/java/br/dev/mhc/nonogram/NonogramApplication.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.nonogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/mhc/nonogram/NonogramApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.mhc.nonogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
